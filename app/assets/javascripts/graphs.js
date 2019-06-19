6701deb32862bcd3c69090132443f87cf9edc437
igarashijyri <fourleafclover688@gmail.com>
2019-06-19 16:51:20 +0900

    htmlタグのロードでより良い方法があった

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -6,3 +6,21 @@ window.onload = function graph_test() {
   {margin: { t: 0 }});
 }
 
+
+// addEventListenerだと
+// 最初のHTMLドキュメントの読み込みと解析が完了した時に、スタイルシートや画像、サブフレームの読み込みが終わるのを待たずに発火
+// onloadより良い時があるらしい？
+
+document.addEventListener('DOMContentLoaded', function(){
+  graph();
+},false);
+
+function graph(){
+  var scores={
+  x: gon.x,
+  y: gon.y, 
+  type: 'scatter'
+  }
+  var data =[scores]
+  Plotly.newPlot('graph', data)
+}
